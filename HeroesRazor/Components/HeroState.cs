8ec1484f74de95fb501f125c473f830865a2d68b
robzelt <robzelt@robzelt.com>
2019-03-09 09:03:04 -0500

    Initial code commit of Razor Components project using .NET Core 3 Preview 3

---
+++ HeroesRazor/Components/HeroState.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Heroes.Shared;
+
+namespace HeroesRazor.Components
+{
+	public class HeroState
+	{
+		public event EventHandler StateChanged;
+		public List<Hero> Heroes { get; set; }
+		public List<Hero> Villains { get; set; }
+
+		private void StateHasChanged()
+		{
+
+			StateChanged?.Invoke(this, EventArgs.Empty);
+		}
+
+		public HeroState()
+		{
+			Heroes = new List<Hero>() {
+	new Hero(){
+	Id= "HeroAslaug",
+	Name= "Aslaug",
+	Description= "warrior queen",
+  },
+  new Hero(){
+	Id = "HeroBjorn",
+	Name = "Bjorn Ironside",
+	Description= "king of 9th century Sweden",
+  },
+  new Hero(){
+	Id = "HeroIvar",
+	Name= "Ivar the Boneless",
+	Description= "commander of the Great Heathen Army",
+  },
+  new Hero(){
+	Id= "HeroLagertha",
+	Name= "Lagertha the Shieldmaiden",
+	Description = "aka Hlaðgerðr",
+  },
+  new Hero(){
+	Id = "HeroRagnar",
+	Name = "Ragnar Lothbrok",
+	Description=  "aka Ragnar Sigurdsson",
+  },
+  new Hero(){
+	Id = "HeroThora",
+	Name= "Thora Town-hart",
+	Description= "daughter of Earl Herrauðr of Götaland",
+  } };
+
+
+			Villains = new List<Hero>() {
+	new Hero() {
+	Id = "VillainMadelyn",
+	Name= "Madelyn",
+	Description= "the cat whisperer",
+  },
+  new Hero(){
+	Id = "VillainHaley",
+	Name = "Haley",
+	Description = "pen wielder",
+  },
+  new Hero(){
+	Id = "VillainElla",
+	Name = "Ella",
+	Description = "fashionista",
+  },
+  new Hero(){
+	Id= "VillainLandon",
+	Name= "Landon",
+	Description = "Mandalorian mauler",
+  }};
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# heroes-razor
+# Tour of Heroes
+
+The Tour of Heroes project was originally created by [John Papa](http://twitter.com/john_papa) to help represent a fundamental app written with Angular. This is a recreation of the application written using ASP.NET Razor Components
+
+by [Rob Zelt](http://twitter.com/robzelt)
+
+The original apps written with Vue and React can be found at at [github.com/johnpapa/heroes-vue](https://github.com/johnpapa/heroes-vue) and [github.com/johnpapa/heroes-react](https://github.com/johnpapa/heroes-react)
+
+## Why
+
+As John previously stated 'One of the most common questions I hear is "which framework is best?". I like to flip this around and ask you "which is best for you?". The best way to know this is to try it for yourself.' I decided to follow his advice and use a non-JavaScript framework to create the same result.
+
+## Live Demos
+
+Hosted in [Azure](https://aka.ms/jp-free)
+
+- [Tour of Heroes with Razor Components](https://zelt-heroes-razor.azurewebsites.net)
+- [Tour of Heroes with Blazor](https://zelt-heroes-blazor.azurewebsites.net)
+- [Tour of Heroes with Angular](https://papa-heroes-angular.azurewebsites.net)
+- [Tour of Heroes with React](https://papa-heroes-react.azurewebsites.net)
+- [Tour of Heroes with Vue](https://papa-heroes-vue.azurewebsites.net)
+
+## Getting Started
+
+[Todo]
+
+
+## Problems or Suggestions
+
+[Open an issue here](/issues)
+
+## Thank You
